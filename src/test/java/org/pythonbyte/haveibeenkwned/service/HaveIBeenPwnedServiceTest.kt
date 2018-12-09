97236e4521ced923bc847d93f1fc86832e5bf099
Mark Nenadov <mark.nenadov@borderconnect.com>
2018-12-09 08:15:06 -0500

    a couple small fixes

--- src/test/java/org/pythonbyte/haveibeenkwned/service/HaveIBeenPwnedServiceTest.kt
+++ src/test/java/org/pythonbyte/haveibeenkwned/service/HaveIBeenPwnedServiceTest.kt
@@ -4,15 +4,15 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertFalse
 
 class HaveIBeenPwnedServiceTest {
+    val haveIBeenPwnedService = HaveIBeenPwnedServiceImpl()
+
     @Test
     fun testIsPasswordPwnedWhenTrue() {
-        val haveIBeenPwnedService = HaveIBeenPwnedServiceImpl()
         assertFalse( haveIBeenPwnedService.isPasswordPwned("password") )
     }
 
     @Test
     fun testIsPasswordPwnedWhenFalse() {
-        val haveIBeenPwnedService = HaveIBeenPwnedServiceImpl()
         assertFalse( haveIBeenPwnedService.isPasswordPwned("fjrelwkfjrelkrgjf393498504358ejgkrjgr") )
     }
 }
